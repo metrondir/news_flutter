ab535f4ff59f30769374a49b088d700ffe74eac7
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2023-09-09 20:50:10 +0200

    first commit

---
+++ lib/features/daily_news/domain/usecases/save_article.dart
@@ -0,0 +1,16 @@
+import 'package:app/core/usecases/usecase.dart';
+import 'package:app/features/daily_news/domain/entities/article.dart';
+import 'package:app/features/daily_news/domain/repository/article_repository.dart';
+
+class SaveArticleUseCase implements UseCase<void,ArticleEntity>{
+  
+  final ArticleRepository _articleRepository;
+
+  SaveArticleUseCase(this._articleRepository);
+  
+  @override
+  Future<void> call({ArticleEntity ? params}) {
+    return _articleRepository.saveArticle(params!);
+  }
+  
+}
